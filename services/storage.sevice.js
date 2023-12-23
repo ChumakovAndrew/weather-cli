4a1256d32d66c7d612e302aa6b04e448b4a60044
DELL5400\root <andrew.chumakov12@gmail.com>
2023-12-23 05:32:21 +0200

    добавлен сервис storage

---
+++ services/storage.sevice.js
@@ -0,0 +1,31 @@
+import { homedir } from "os";
+import { promises, existsSync } from "fs"
+import { join } from "path";
+
+const {writeFile, mkdir, readFile} = promises
+
+const pathCliDir = join(homedir(), '/AppData/Local/weather_cli');
+const filePath = join(pathCliDir, 'weather_data.json')
+
+
+
+const saveKeyValue = async (key, value) => {
+    let data = {}
+
+    if(existsSync(filePath)){
+        const file = await readFile(filePath);
+        data = JSON.parse(file);
+        data[key] = value;
+        await writeFile(filePath, JSON.stringify(data));
+    }else{
+        data[key] = value;
+        createFile(pathCliDir, filePath, data);
+    }
+}
+
+const createFile = async (pathDir, filePath, data) => {
+    await mkdir(pathDir, { recursive: true });
+    await writeFile(filePath, JSON.stringify(data));
+}
+
+export {saveKeyValue}
